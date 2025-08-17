d0ea4257a1e3220fb42b36dd82e9b8bf57a72612
AymericHenouille <aymeric.henouille@gmail.com>
2025-08-17 23:24:11 +0200

    tests: function_call tests

--- lua/origami/format/fold.lua
+++ lua/origami/format/fold.lua
@@ -6,9 +6,9 @@ local M = {}
 function M.setup(opts)
   local find_block_node = require("origami.options.block_node_option").setup_find_block_node(opts)
   return function(node)
-    local block_node, options = find_block_node(node)
+    local block_node, block_node_option = find_block_node(node)
     if not block_node then return end
-    local lines = options.fold(block_node)
+    local lines = block_node_option.fold(block_node)
     for _, line in ipairs(lines) do
       print(line)
     end
--- lua/origami/options/lua/function_call.lua
+++ lua/origami/options/lua/function_call.lua
@@ -2,10 +2,37 @@
 local M = {}
 
 function M.fold(node)
-  return { "test" }
+  local lines = {}
+  for child in node:iter_children() do
+    local child_type = child:type()
+    if child_type == "arguments" then
+      for argument in child:iter_children() do
+        local argument_type = argument:type()
+        print("argument: " .. argument_type)
+      end
+    else
+      print(child_type)
+    end
+  end
+  return lines
 end
 
 function M.unfold(node)
+
+  local function test(a, b, c, d)
+
+  end
+
+  test(
+    "1", --comment first message
+    test(1, 2, 3, 4),
+    --[[comment]]{
+      test = ""
+    },
+    function()
+
+    end,
+  )
   return {}
 end
 
